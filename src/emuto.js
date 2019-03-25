f21d3a5b8dccc592c335daf57b4fdc2dbc0ae8a3
Dániel Kántor <git@daniel-kantor.com>
2019-03-25 19:59:24 +0100

    refactor: extract parseInput to a separate file

--- src/emuto.js
+++ src/emuto.js
@@ -2,37 +2,7 @@ const beautify = require('json-beautify')
 const emuto = require('node-emuto')
 const highlight = require('highlight-es')
 const validateFlags = require('./validate-flags.js')
-
-const parseInput = (str, type, inputDelimiter, inputFeatures) => {
-  const columns = inputFeatures.includes('head')
-
-  if (!str) {
-    return null
-  }
-  if (type === 'json') {
-    return JSON.parse(str)
-  }
-  if (type === 'raw') {
-    return str.split('\n')
-  }
-
-  if (type === 'csv') {
-    return require('csv-parse/lib/sync')(str, {columns})
-  }
-
-  if (type === 'tsv') {
-    return require('csv-parse/lib/sync')(str, {delimiter: '\t', columns})
-  }
-
-  if (type === 'dsv') {
-    return require('csv-parse/lib/sync')(str, {
-      delimiter: inputDelimiter,
-      columns,
-    })
-  }
-
-  throw new Error("Input format '" + type + "' is unkown")
-}
+const parseInput = require('./parse-input.js')
 
 const serializeToJSON = (output, format, ugly, color) => {
   const serializer = ugly ?
