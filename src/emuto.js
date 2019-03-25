d3d6ca2f1cb431749666788f312cec75be8a7fbf
Dániel Kántor <git@daniel-kantor.com>
2019-03-25 19:56:35 +0100

    refactor: extract validateFlags to a separate file

--- src/emuto.js
+++ src/emuto.js
@@ -1,6 +1,7 @@
 const beautify = require('json-beautify')
 const emuto = require('node-emuto')
 const highlight = require('highlight-es')
+const validateFlags = require('./validate-flags.js')
 
 const parseInput = (str, type, inputDelimiter, inputFeatures) => {
   const columns = inputFeatures.includes('head')
@@ -52,23 +53,6 @@ const serializer = (output, format, ugly, color) => {
   }
 
   throw new Error("Output format '" + format + "' is unkown")
-}
-
-const validateFlags = flags => {
-  const asksForHeader = (flags['input-feature'] || []).includes('head')
-  const supportsHeader = ['csv', 'dsv', 'tsv'].includes(flags.input)
-
-  if (flags.input === 'dsv' && !flags['input-delimiter']) {
-    throw new Error('You have to specify a delimiter to use dsv input format')
-  }
-
-  if (flags.input !== 'dsv' && flags['input-delimiter']) {
-    throw new Error('Input delimiter is only valid with dsv input format')
-  }
-
-  if (asksForHeader && !supportsHeader) {
-    throw new Error('Header is only supported for csv, tsv, and dsv inputs')
-  }
 }
 
 const createEmutoCliCommand = ({getStdin, fs}) => {
